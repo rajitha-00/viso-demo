b0fda92fbaf79bdd02f4b70db3f08ded1a270571
rajitha-00 <rajithatw07102@gmail.com>
2023-07-03 13:59:04 +0530

    sections and constants init

---
+++ src/constants/index.jsx
@@ -0,0 +1,46 @@
+export const servicesArray = [
+    {
+        id: '1',
+        imgUrl:'',
+        title: 'Drafting Primary Legislations',
+        content: 'Creating new laws by government officials or drafters.'
+    },
+    {
+        id: '2',
+        imgUrl:'',
+        title: 'Amending Legislation',
+        content: 'Modifying existing laws to update, correct, eorros, or address new circumstances.'
+    },
+    {
+        id: '3',
+        imgUrl:'',
+        title: 'Drafting Private Member Bills',
+        content: 'Legislators proposing laws outside the governments purview. '
+    },
+];
+export const projectsArray = [
+    {
+        id: '1',
+        imgUrl:'',
+        title: 'Blog Name Here',
+        content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam aspernatur exercitationem non officia debitis consequuntur et aliquid labore distinctio quia hic facilis quaerat optio, perferendis, omnis voluptatibus impedit laudantium quod?'
+    },
+    {
+        id: '2',
+        imgUrl:'',
+        title: 'Blog Name Here',
+        content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam aspernatur exercitationem non officia debitis consequuntur et aliquid labore distinctio quia hic facilis quaerat optio, perferendis, omnis voluptatibus impedit laudantium quod?'
+    },
+    {
+        id: '3',
+        imgUrl:'',
+        title: 'Blog Name Here',
+        content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam aspernatur exercitationem non officia debitis consequuntur et aliquid labore distinctio quia hic facilis quaerat optio, perferendis, omnis voluptatibus impedit laudantium quod?'
+    },
+    {
+        id: '4',
+        imgUrl:'',
+        title: 'Blog Name Here',
+        content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam aspernatur exercitationem non officia debitis consequuntur et aliquid labore distinctio quia hic facilis quaerat optio, perferendis, omnis voluptatibus impedit laudantium quod?'
+    },
+];
---
+++ src/views/Landing.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from '../sections/Hero'
+import About from '../sections/About'
+import Services from '../sections/Services'
+import Projects from '../sections/Projects'
+import Contact from '../sections/Contact'
+
+const Landing = () => {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Services />
+      <Projects />
+      <Contact />
+    </>
+  )
+}
+
+export default Landing
