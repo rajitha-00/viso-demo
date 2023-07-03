683f23ffc3d0885500d9dddea5e4fafc8daf0537
rajitha-00 <rajithatw07102@gmail.com>
2023-07-03 22:56:14 +0530

    contact done

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,11 +1,93 @@
 import React from 'react'
-
+import ContactFig from '../assets/contact.png'
 const Contact = () => {
   return (
     <>
-      <section className="contact" id='contact'>
-        <h1>contact</h1>
+      <div className="wrapper-contact">
+
+      <section className="contact flex items-center justify-center" id='contact'>
+        <div className="contactCard bg-white w-1/2 px-20 py-10">
+
+          <h1 className='lg:text-3xl text-blue-950'>Get In Touch</h1>
+          <div className="flex justify-between">
+
+            <div className="left-side">
+              <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+              <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                <div>
+
+                  <div className="mt-2.5">
+                    <input
+                      type="text"
+                      name="first-name"
+                      id="first-name"
+                      autoComplete="given-name"
+                      placeholder='Name'
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+                <div>
+
+                  <div className="mt-2.5">
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      autoComplete="email"
+                      placeholder='Email "example@example.com"'
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                
+                  <div className="mt-2.5">
+                    <input
+                      type="text"
+                      name="subject"
+                      id="subject"
+                      autoComplete="organization"
+                      placeholder='Subject'
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+
+                <div className="sm:col-span-2">
+
+                  <div className="mt-2.5">
+                    <textarea
+                      name="message"
+                      id="message"
+                      placeholder='Message...'
+                      rows={4}
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      defaultValue={''}
+                    />
+                  </div>
+                </div>
+
+              </div>
+              <div className="mt-10">
+                <button
+                  type="submit"
+                  className="block px-14 rounded-md bg-orange-400 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Send
+                </button>
+              </div>
+              </form>
+
+            </div>
+            <div className="right-side">
+              <img src={ContactFig} alt="" />
+            </div>
+          </div>
+        </div>
       </section>
+      </div>
     </>
   )
 }
