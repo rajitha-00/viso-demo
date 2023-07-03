00c7a47b18e0335bed18ebe58ddb033f2935f861
rajitha-00 <rajithatw07102@gmail.com>
2023-07-03 14:18:58 +0530

    assets init

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,19 +1,19 @@
 export const servicesArray = [
     {
         id: '1',
-        imgUrl:'',
+        imgUrl:'/services_1.png',
         title: 'Drafting Primary Legislations',
         content: 'Creating new laws by government officials or drafters.'
     },
     {
         id: '2',
-        imgUrl:'',
+        imgUrl:'/services_2.png',
         title: 'Amending Legislation',
         content: 'Modifying existing laws to update, correct, eorros, or address new circumstances.'
     },
     {
         id: '3',
-        imgUrl:'',
+        imgUrl:'/services_3.png',
         title: 'Drafting Private Member Bills',
         content: 'Legislators proposing laws outside the governments purview. '
     },
@@ -21,25 +21,25 @@ export const servicesArray = [
 export const projectsArray = [
     {
         id: '1',
-        imgUrl:'',
+        imgUrl:'/project_1.png',
         title: 'Blog Name Here',
         content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam aspernatur exercitationem non officia debitis consequuntur et aliquid labore distinctio quia hic facilis quaerat optio, perferendis, omnis voluptatibus impedit laudantium quod?'
     },
     {
         id: '2',
-        imgUrl:'',
+        imgUrl:'/project_1.png',
         title: 'Blog Name Here',
         content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam aspernatur exercitationem non officia debitis consequuntur et aliquid labore distinctio quia hic facilis quaerat optio, perferendis, omnis voluptatibus impedit laudantium quod?'
     },
     {
         id: '3',
-        imgUrl:'',
+        imgUrl:'/project_1.png',
         title: 'Blog Name Here',
         content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam aspernatur exercitationem non officia debitis consequuntur et aliquid labore distinctio quia hic facilis quaerat optio, perferendis, omnis voluptatibus impedit laudantium quod?'
     },
     {
         id: '4',
-        imgUrl:'',
+        imgUrl:'/project_1.png',
         title: 'Blog Name Here',
         content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam aspernatur exercitationem non officia debitis consequuntur et aliquid labore distinctio quia hic facilis quaerat optio, perferendis, omnis voluptatibus impedit laudantium quod?'
     },
